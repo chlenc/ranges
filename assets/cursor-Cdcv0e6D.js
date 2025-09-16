28ff03f5841ebac40640e517d902ce0de835f8d9
alexey <alexnagorny.an@gmail.com>
2025-09-17 01:06:40 +0200

    Updates

---
+++ assets/cursor-Cdcv0e6D.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-CHSm3xv_.js";import"./index-E8K9zNcO.js";import"./index.es-6SUjmkus.js";import"./index-nibyPLVP.js";import"./fallback-Dvo0UqNy.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
