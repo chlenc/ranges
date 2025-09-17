ec29f2221d4ad57fd8206f07bf8c1243fe2e803f
alexey <alexnagorny.an@gmail.com>
2025-09-17 22:44:27 +0200

    Updates

--- assets/arrow-right-D2SHJcp9.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-NAQ1bMIC.js";import"./index-MjOix_bB.js";import"./index.es-DWQJJOdP.js";import"./index-nibyPLVP.js";import"./fallback-CcyCN-Qp.js";const p=o`<svg fill="none" viewBox="0 0 14 15">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M1 7.99a1 1 0 0 1 1-1h7.58L7.12 4.53A1 1 0 1 1 8.54 3.1l4.16 4.17a1 1 0 0 1 0 1.41l-4.16 4.17a1 1 0 1 1-1.42-1.41l2.46-2.46H2a1 1 0 0 1-1-1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{p as arrowRightSvg};
